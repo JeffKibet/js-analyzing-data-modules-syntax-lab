9eacfdacb8e0b28fd0969a9077a9d95175afc2b0
jeff <jefferykibet@gmail.com>
2026-07-05 19:15:56 +0300

    Remove combineUsers function implementation from index.js

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-function combineUsers(...userArrays) {
-  // Merge all the incoming arrays into one flat array
-  const mergedUsers = [].concat(...userArrays);
-
-  // Record the date/time of the merge
-  const mergeDate = new Date().toISOString();
-
-  return {
-    mergeDate,
-    mergedUsers,
-  };
-}
 
 module.exports = {
   ...(typeof combineUsers !== "undefined" && { combineUsers }),
