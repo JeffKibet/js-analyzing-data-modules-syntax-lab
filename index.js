43263cdb8bbc2f72fbd14558001475c11a350e09
jeff <jefferykibet@gmail.com>
2026-07-05 19:14:10 +0300

    Refactor combineUsers function to rename variable and improve comments

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 function combineUsers(...userArrays) {
-  const allUsers = [].concat(...userArrays);
+  // Merge all the incoming arrays into one flat array
+  const mergedUsers = [].concat(...userArrays);
+
+  // Record the date/time of the merge
   const mergeDate = new Date().toISOString();
 
   return {
     mergeDate,
-    users: allUsers
+    mergedUsers,
   };
 }
 
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+  ...(typeof combineUsers !== "undefined" && { combineUsers }),
 };
