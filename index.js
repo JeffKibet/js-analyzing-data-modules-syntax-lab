6705c536f880c74cc5d378e72b7e3ab6f82ab37c
jeff <jefferykibet@gmail.com>
2026-07-05 19:26:52 +0300

    Add combineUsers function to merge user arrays and include merge date

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+function combineUsers(...userArrays) {
+  const users = [].concat(...userArrays);
+
+  const today = new Date();
+  const merge_date = `${today.getMonth() + 1}/${today.getDate()}/${today.getFullYear()}`;
+
+  return {
+    merge_date,
+    users,
+  };
+}
 
 module.exports = {
   ...(typeof combineUsers !== "undefined" && { combineUsers }),
