fecef06f13d9c32651467cb9156e0ba662369cc3
jeff <jefferykibet@gmail.com>
2026-07-05 18:59:02 +0300

    Refactor combineUsers function to include mergeDate in the return object

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+function combineUsers(...userArrays) {
+  const allUsers = [].concat(...userArrays);
+  const mergeDate = new Date().toISOString();
 
-
+  return {
+    mergeDate,
+  }
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
